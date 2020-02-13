9e54a035fbfa905c5cd4617728e03ec228991c82
ryanhossain9797 <ryanhossain9797@gmail.com>
2020-02-14 01:44:23 +0600

    Implement entry and detail view

---
+++ app/src/main/java/com/example/flickrbrowser/DetailsActivity.kt
@@ -0,0 +1,17 @@
+package com.example.flickrbrowser
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_details.*
+
+class DetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+        setSupportActionBar(toolbar)
+    }
+
+}
